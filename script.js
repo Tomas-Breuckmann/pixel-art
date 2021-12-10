f1e07b03d1346921cbb58e106cdaef4af18ca9e5
Tomas Breuckmann <tomas.yoga@yahoo.com>
2021-12-10 16:04:19 -0300

    pixel art - criterio 4 - npm test ok

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function quadroDePixel(numero){
     let board=document.getElementById('pixel-board')
-    numero=numero*numero
-    for (i=1;i<=numero;i+=1){
-        // let linha=document.createElement('div')
-        let item=document.createElement('div')
-        item.className='pixel'
-        item.style.backgroundColor='white'
-        board.appendChild(item)
+    for (let i=0;i<numero;i+=1){
+        let pixelBoardSection=document.createElement('div')
+        pixelBoardSection.className='pixel-board-section'
+        board.appendChild(pixelBoardSection)
+        for (j=1;j<=numero;j+=1){
+            let item=document.createElement('div')
+            item.className='pixel'
+            pixelBoardSection.appendChild(item)
+        }
     }
 }
 quadroDePixel(5)
