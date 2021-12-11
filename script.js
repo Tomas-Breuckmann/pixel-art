a01b52201bda32b8968b90a6467709b491ea19a6
Tomas Breuckmann <tomas.yoga@yahoo.com>
2021-12-10 23:21:29 -0300

    pixel art - criterio 8 - npm test ok

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// window.onload=function(){
+//     function pintaBlack(event){
+//         let pix=document.getElementById('pixel-board')
+//         pix.addEventListener('click',function(event){
+//             event.target.style.backgroundColor='black'
+//         })
+//     }
+//     pintaBlack()
+// }
+
 function quadroDePixel(numero){
     let board=document.getElementById('pixel-board')
     board.style.width='210px'
@@ -12,17 +22,21 @@ function quadroDePixel(numero){
 }
 quadroDePixel(5)
 
-
-
 function selecionaCor(event){
     let aCor=document.querySelector('.selected')
-    console.log(aCor)
     aCor.classList.remove('selected')
     event.target.classList.add('selected')
-    console.log(aCor)
 }
-
 document.getElementById('cor1').addEventListener('click',selecionaCor)
 document.getElementById('cor2').addEventListener('click',selecionaCor)
 document.getElementById('cor3').addEventListener('click',selecionaCor)
 document.getElementById('cor4').addEventListener('click',selecionaCor)
+
+// As linhas 36 a 43 são de Danillo Goncalves Batista
+const pixels = document.querySelectorAll('.pixel');
+for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', function(e) {
+        let propriedades=document.querySelector('.selected').id
+        e.target.id = propriedades;
+    })
+}
